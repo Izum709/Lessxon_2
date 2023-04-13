ed583c69aeaec351fe4814175232d1777a099dcd
Alexandr Vinogradov <izum709@.com>
2023-04-13 19:37:15 +0300

    Add arrey and while

---
+++ Arrey_Wile/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arrey ={41, 24, 35, 4, 75, 60, 7, 88, 94, 140};
+int n = arrey.Length;
+int find = 140;
+int index = 0;
+while (index < n)
+{
+    if (arrey[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
