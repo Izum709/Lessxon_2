4811406642d06afda7623309b5a7d1f129593388
Alexandr Vinogradov <izum709@.com>
2023-04-20 14:57:50 +0300

    add Arrey Randum

---
+++ Arrey_Randum/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArrey(int[] Collection) 
+{
+    int Length = Collection.Length;
+    int index = 0;
+    while (index < Length) 
+    {
+        Collection[index] = new Random().Next(1, 10);
+
+        index++;
+
+    }
+}
+
+void PrintArrey( int[] col) 
+{
+    int count = col.Length;
+    int position = 0;
+    while(position <count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+ 
+ int[] arrey = new int[10];
+ FillArrey(arrey);
+ PrintArrey(arrey);
